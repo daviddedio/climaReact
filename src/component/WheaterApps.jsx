b9ef6c97047b239891198c52758650e6a31d902e
daviddedio <ddedio@gmail.com>
2025-01-11 17:13:27 -0300

    Aplicacion finalizada

--- src/component/WheaterApps.jsx
+++ src/component/WheaterApps.jsx
@@ -2,28 +2,57 @@ import { useState } from "react"
 
 export const WheaterApps = () => {
 
-    const [ciudad, setCiudad] = useState('')
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const ApiKey = '7c4ba064b92222cb8c9a3354cbaf8617'
+    const difkelvin = 273.15
 
-    const handlerCiudad = (e) =>{
+    const [ciudad, setCiudad] = useState('')
+    const [dataClima, setDataClima] = useState(null)
+
+    const handleCambioCiudad = (e) => {
         setCiudad(e.target.value)
     }
 
-    const handlerSubmit = (e) =>{
+    const handlerSubmit = (e) => {
+        console.log('aca')
         e.preventDefault()
         if (ciudad.length > 0) fetchClima()
+    }
+
+    const fetchClima = async () => {
+        try {
+            const response = await fetch(`${urlBase}?q=${ciudad}&appid=${ApiKey}`)
+            const data = await response.json()
+            setDataClima(data)
+        } catch (error) {
+            console.error(`Ocurrio el siguiente problema > ${error}`)
+        }
     }
 
     return (
         <div className="container">
             <h1>Aplicacion del clima</h1>
-            <form action="" onSubmit={handlerSubmit}>
-                <input 
-                type="text"
-                value={ciudad}
-                onClick={handlerCiudad}
+            <form onSubmit={handlerSubmit}>
+                <input
+                    type="text"
+                    value={ciudad}
+                    onChange={handleCambioCiudad}
                 />
                 <button type="submit">Buscar</button>
             </form>
+            {
+                dataClima && (
+                    <div class="card">
+                        <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} alt="clima"/>
+                            <div class="cont">
+                                <h3><b>{dataClima.name}</b></h3>
+                                <p><b>Condiciones meteorologicas:</b> {dataClima.weather[0].description}</p>
+                                <p><b>Temperatura:</b> {parseInt(dataClima.main.temp - difkelvin)}C - Max {parseInt(dataClima.main.temp_max - difkelvin)}C / Min {parseInt(dataClima.main.temp_min - difkelvin)}C</p>
+                                <p><b>Viento:</b> {dataClima.wind.speed} - Direccion: {dataClima.wind.grados} </p>
+                            </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
